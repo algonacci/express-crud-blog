bfec4d27360f1d62e3c1d5872f7ea852184e94bd
Eric Julianto <algonacci1@gmail.com>
2023-02-04 09:27:53 +0700

    Initial setup

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+const path = require("path");
 const express = require("express");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "php-crud-blog",
+});
 
 app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", "./public");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index.ejs");
 });
 
 module.exports = app;
